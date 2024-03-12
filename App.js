c7957253582a042368d619498f274f13a9072e19
Phillipe <ph.stos.udia@gmail.com>
2024-03-12 20:19:58 -0300

    Branch React-3

--- App.js
+++ App.js
@@ -11,26 +11,3 @@ export default function App() {
     </View>
   );
 }
-
-export default function AppLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          // Essa guia não aparecerá na barra de guias.
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="[user]"
-        options={{
-          // Garanta que a guia sempre aponte para o mesmo href.
-          href: '/evanbacon',
-          // OU você pode usar o objeto Href:
-          // href: { pathname: '/[user]', params: { user: 'evanbacon' } },
-        }}
-      />
-    </Tabs>
-  );
-}
